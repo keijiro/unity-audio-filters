aeea579e53fb185e0910d4dd7ddfa01e305b8116
Keijiro Takahashi <keijiro@gmail.com>
2013-07-20 10:08:35 +0900

    Use a look-up table in SinWave class.

--- Assets/Standard Assets/Stk/SineWave.cs
+++ Assets/Standard Assets/Stk/SineWave.cs
@@ -1,13 +1,44 @@
+// Not-so-memory-efficient sine wave class.
+// Written by Keijiro Takahashi
+
 namespace Stk
 {
     public class SineWave
     {
-        float phase;
+        const int TableSize = 1024; // must be a power of 2.
+        const float Pi2 = 6.28318530718f;
+        static float[] table;
+        float time01;
         float delta;
-        float lastOut;
 
-        public float LastOut {
-            get { return lastOut;}
+        static void InitializeTable ()
+        {
+            table = new float[TableSize];
+            for (var i = 0; i < TableSize; i++) {
+                table [i] = (float)System.Math.Sin (i * Pi2 / TableSize);
+            }
+        }
+
+        static float LookUpTable (float time01)
+        {
+            time01 *= TableSize;
+            var i1 = (int)time01;
+            var i2 = (i1 + 1) & (TableSize - 1);
+            float fraction = time01 - i1;
+            return 0.5f * (table [i1] * (1.0f - fraction) + table [i2] * fraction);
+        }
+        
+        public SineWave ()
+        {
+            if (table == null)
+                InitializeTable ();
+        }
+
+        public SineWave (float frequency)
+        {
+            if (table == null)
+                InitializeTable ();
+            Frequency = frequency;
         }
 
         public float Frequency {
@@ -16,10 +47,10 @@ namespace Stk
 
         public float Tick ()
         {
-            lastOut = UnityEngine.Mathf.Sin (phase * 6.28318530718f);
-            phase += delta;
-            phase -= (int)phase;
-            return lastOut;
+            float output = LookUpTable (time01);
+            time01 += delta;
+            time01 -= (int)time01;
+            return output;
         }
     }
 }
